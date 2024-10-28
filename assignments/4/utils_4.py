dac80cc2e66961e71dbb4ad7f4ca3d3a1632ec77
SohamVaishnav <soham.vaishnav@research.iiit.ac.in>
2024-10-28 17:08:47 +0530

    one hot encoding done

--- assignments/4/utils_4.py
+++ assignments/4/utils_4.py
@@ -107,6 +107,11 @@ class MultiMNISTDataset(object):
                     self._valid_labels.extend([len(file.split('_')[-1].removesuffix('.png')) for file in files])
                 else:
                     self._valid_labels.extend([0 for file in files])
+            elif (task == 'multi_label'):
+                if ([len(file.split('_')) > 1 for file in files] == [True for file in files]):
+                    self._valid_labels.extend([int(file.split('_')[0])+1 for file in files])
+                else:
+                    self._valid_labels.extend([0 for file in files])
         
         for i in range(len(self._valid_images_paths)):
             self._valid_images.append(cv2.imread(self._valid_images_paths[i], cv2.IMREAD_GRAYSCALE))
@@ -124,6 +129,11 @@ class MultiMNISTDataset(object):
                     self._test_labels.extend([len(file.split('_')[-1].removesuffix('.png')) for file in files])
                 else:
                     self._test_labels.extend([0 for file in files])
+            elif (task == 'multi_label'):
+                if ([len(file.split('_')) > 1 for file in files] == [True for file in files]):
+                    self._test_labels.extend([int(file.split('_')[0])+1 for file in files])
+                else:
+                    self._test_labels.extend([0 for file in files])
         
         for i in range(len(self._test_images_paths)):
             self._test_images.append(cv2.imread(self._test_images_paths[i], cv2.IMREAD_GRAYSCALE))
@@ -136,10 +146,64 @@ class MultiMNISTDataset(object):
         
         pass
     
-    def getLabels(self) -> None:
+    def getLabels(self, task: str) -> None:
         '''
         Returns the labels for the dataset.
+
+        Parameters:
+            task: str = the task to get the labels for.
         '''
-        labels = np.unique(self._train_labels + self._valid_labels + self._test_labels)
-        self._labels = labels
+        if (task == 'num_digits'):
+            self._labels = np.unique(self._train_labels + self._valid_labels + self._test_labels)
+        elif (task == 'multi_label'):
+            labels = np.unique(self._train_labels + self._valid_labels + self._test_labels)
+            self._labels = np.zeros((len(labels), 33))
+            for i in range(len(labels)):
+                temp = str(labels[i]-1)
+                if (len(temp) == 1 or temp == '-1'):
+                    self._labels[i][int(temp)+1] = 1
+                    self._labels[i][11] = 1
+                    self._labels[i][22] = 1
+                elif (len(temp) == 2 and temp != '-1'):
+                    self._labels[i][int(temp[0])+1] = 1
+                    self._labels[i][11+int(temp[1])+1] = 1
+                    self._labels[i][22] = 1
+                elif (len(temp) == 3):
+                    self._labels[i][int(temp[0])+1] = 1
+                    self._labels[i][11+int(temp[1])+1] = 1
+                    self._labels[i][22+int(temp[2])+1] = 1
         return self._labels
+
+    def OneHotEncoder(self, curr_labels: list) -> np.ndarray:
+        ''' 
+        One hot encodes the labels.
+
+        Parameters:
+            curr_labels: list = the labels to one hot encode.
+        '''
+        labels = np.zeros((len(curr_labels), 33))
+        for i in range(len(curr_labels)):
+            temp = str(curr_labels[i]-1)
+            if (len(temp) == 1 or temp == '-1'):
+                labels[i][int(temp)+1] = 1
+                labels[i][11] = 1
+                labels[i][22] = 1
+            elif (len(temp) == 2 and temp != '-1'):
+                labels[i][int(temp[0])+1] = 1
+                labels[i][11+int(temp[1])+1] = 1
+                labels[i][22] = 1
+            elif (len(temp) == 3):
+                labels[i][int(temp[0])+1] = 1
+                labels[i][11+int(temp[1])+1] = 1
+                labels[i][22+int(temp[2])+1] = 1
+        return labels
+    
+# DL = MultiMNISTDataset(32, RawDataDIR)
+# DL.load_mnist_data('multi_label')
+# labels = DL.getLabels('multi_label')
+# labs = DL.OneHotEncoder(DL._test_labels)
+
+# df = pd.DataFrame(np.column_stack((labs, np.array(DL._test_labels)-1)), columns=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 
+#                                                                             '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21',
+#                                                                             '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', 'digits'])
+# df.to_csv(os.path.join(PreProcessDIR, 'multi_label_test_labels.csv'), index=False)
