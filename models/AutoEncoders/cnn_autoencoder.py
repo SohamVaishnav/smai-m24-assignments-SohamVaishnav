628994d3cc91801e1011b6a04151c0311ebb2826
SohamVaishnav <soham.vaishnav@research.iiit.ac.in>
2024-11-01 21:26:24 +0530

    cnn_autoencoder final version

--- models/AutoEncoders/cnn_autoencoder.py
+++ models/AutoEncoders/cnn_autoencoder.py
@@ -25,33 +25,80 @@ from models.cnn import multilabel_cnn
 from models.AutoEncoders import auto_encoder, cnn_autoencoder, pca_autoencoder
 
 class CnnAutoencoder(nn.Module):
-    def __init__(self):
+    '''
+    CNN AutoEncoder class.
+    '''
+    def __init__(self, config):
+        '''
+        Initialising the class.
+
+        Parameters:
+            config: dictionary containing the configuration parameters 
+        '''
         super(CnnAutoencoder, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(True),
-            nn.Conv2d(32, 64, kernel_size=7)
-        )
-        self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, kernel_size=7),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=1, output_padding=1),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(8, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
-        )
+        self.config = config
+        self.in_channels = config['in_channels']
+        self.encoder_layers = config['encoder_layers']
+        self.decoder_layers = config['decoder_layers']
+        self.lr = config['lr']
+
+        encoder_layers = []
+        length = len(self.encoder_layers)
+        i = 0
+        in_channels = self.in_channels
+        for out_channels, kernel_size, stride, padding in self.encoder_layers:
+            encoder_layers.append(nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding))
+            if (i != length - 1):
+              if (self.activation == 'relu'):
+                encoder_layers.append(nn.ReLU(True))
+            elif (self.activation == 'sigmoid'):
+                encoder_layers.append(nn.Sigmoid())
+            elif (self.activation == 'tanh'):
+                encoder_layers.append(nn.Tanh())
+            in_channels = out_channels
+            i += 1
+        self.encoder = nn.Sequential(*encoder_layers)
+
+        decoder_layers = []
+        length = len(self.decoder_layers)
+        i = 0
+        for out_channels, kernel_size, stride, padding, output_padding in self.decoder_layers:
+            decoder_layers.append(nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, output_padding))
+            if (i != length - 1):
+                if (self.activation == 'relu'):
+                    decoder_layers.append(nn.ReLU(True))
+                elif (self.activation == 'sigmoid'):
+                    decoder_layers.append(nn.Sigmoid())
+                elif (self.activation == 'tanh'):
+                    decoder_layers.append(nn.Tanh())
+            in_channels = out_channels
+            i += 1
+        self.decoder = nn.Sequential(*decoder_layers)
 
     def encode(self, x):
+        '''
+        Encode the input data.
+
+        Parameters:
+            x: input data 
+        '''
         return self.encoder(x)
 
     def decode(self, z):
+        '''
+        Decode the latent space representation.
+
+        Parameters:
+            z: latent space representation
+        '''
         return self.decoder(z)
 
     def forward(self, x):
+        '''
+        Forward pass of the network.
+
+        Parameters:
+            x: input data 
+        '''
         latent = self.encode(x)
         return self.decode(latent)
